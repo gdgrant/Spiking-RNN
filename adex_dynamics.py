10a12cfa58ea4ef7608464ab32cce2a5b3c4224d
nmasse <masse@uchicago.edu>
2019-06-03 09:30:19 -0500

    Minor fix

--- adex_dynamics.py
+++ adex_dynamics.py
@@ -1,7 +1,7 @@
 from imports import *
 from parameters import par
 
-def calculate_dynamics(prev_eps, st, x, z, z_prev, z_prev_prev, syn_x_prev, syn_u_prev, h, h_prev, con_dict, eff_var, var_dict):
+def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_prev, syn_u_prev, h, h_prev, con_dict, eff_var, var_dict):
 	""" Calculate the dynamics of the model
 		prev_eps = the epsilons of the previous time step
 		st       = dict holding all elements of model state
@@ -29,6 +29,7 @@ def calculate_dynamics(prev_eps, st, x, z, z_prev, z_prev_prev, syn_x_prev, syn_
 	# in preparation for outer products and broadcasting
 	z            = z[:,cp.newaxis,:]
 	z_prev       = z_prev[:,:,cp.newaxis]
+	x_prev       = x_prev[:,:,cp.newaxis]
 	z_prev_prev  = z_prev_prev[:,:,cp.newaxis]
 	h_prev       = h_prev if par['full_derivative'] else 0.
 
@@ -45,17 +46,14 @@ def calculate_dynamics(prev_eps, st, x, z, z_prev, z_prev_prev, syn_x_prev, syn_
 	one_minus_z_dt_over_C = one_minus_z * dt_over_C
 	one_minus_z_dt_mu_over_C = one_minus_z_dt_over_C * c['mu']
 
-	if par['dv_approx']:
-		# Use approximation of exponential of voltage in gradient
-		lin_v = (v - c['V_T'])/c['D']
-		eps_dyn_v = lin_v
-	else:
-		# Use full derivative of voltage in gradient
-		exp_v_minus_one = cp.exp((v-c['V_T'])/c['D'])-1
-		exp_v_minus_one = cp.minimum(1., exp_v_minus_one)
-		eps_dyn_v = exp_v_minus_one
+	# Use full derivative of voltage in gradient
+	exp_v_minus_one = cp.exp((v-c['V_T'])/c['D'])-1
+	#exp_v_minus_one = cp.minimum(1., exp_v_minus_one)
+	exp_v_minus_one = cp.clip(exp_v_minus_one, -1., 1.)
+	eps_dyn_v = exp_v_minus_one
 
-	adex_voltage_dvdv = one_minus_z*(1+dt_g_over_C*eps_dyn_v)
+	adex_voltage_dvdv = one_minus_z*(1 + dt_g_over_C*eps_dyn_v)
+
 
 	# Set up new epsilon recording
 	eps = {}
@@ -63,7 +61,6 @@ def calculate_dynamics(prev_eps, st, x, z, z_prev, z_prev_prev, syn_x_prev, syn_
 	eps['rec'] = {}
 
 	### Update input epsilons
-
 	eps['inp']['v'] = \
 		  prev_eps['inp']['v']  * adex_voltage_dvdv \
 		- prev_eps['inp']['w']  * one_minus_z_dt_over_C \
@@ -76,7 +73,6 @@ def calculate_dynamics(prev_eps, st, x, z, z_prev, z_prev_prev, syn_x_prev, syn_
 	eps['inp']['ia'] = \
 		  prev_eps['inp']['ia'] * c['beta'] \
 		+ one_minus_beta * x
-
 
 	### Update recurrent epsilons
 
@@ -100,15 +96,16 @@ def calculate_dynamics(prev_eps, st, x, z, z_prev, z_prev_prev, syn_x_prev, syn_
 		- prev_eps['rec']['su'] * syn_x * z_prev
 
 	eps['rec']['su'] = \
-		  prev_eps['rec']['su'] * (1 - con_dict['alpha_std'] - con_dict['U']*z_prev)
-
+		  prev_eps['rec']['su'] * (1 - con_dict['alpha_stf'] - con_dict['U']*z_prev) 
 
 	### Second-order corrections to recurrent epsilons
-	term1 = one_minus_beta * h_prev * one_minus_z_dt_mu_over_C * syn_x_prev * syn_u_prev * z_prev_prev * d_eff_weights_raw_weights
+	"""
+	term1_z = one_minus_beta * h_prev * one_minus_z_dt_mu_over_C * syn_x_prev * syn_u_prev * z_prev_prev * d_eff_weights_raw_weights
+	term1_x = one_minus_beta * h_prev * one_minus_z_dt_mu_over_C * x_prev
 	term2 = one_minus_beta * syn_u  * syn_x * eff_var['W_rnn'][cp.newaxis,:,:]
-	eps['rec']['ir'] += cp.einsum('bij,bjk->bik', term1, term2)
 
-	#eps['rec']['sx'] += h_prev * syn_x * syn_u
-	#eps['rec']['su'] += h_prev * con_dict['U'] * (1 - syn_u)
+	eps['rec']['ir'] += cp.einsum('bij,bjk->bik', term1_z, term2)
+	eps['inp']['ia'] += cp.einsum('bij,bjk->bik', term1_x, term2)
+	"""
 
 	return eps
