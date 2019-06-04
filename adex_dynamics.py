636d1b852ba43c901dc5e6f02615fdfa0e57fed0
nmasse <masse@uchicago.edu>
2019-06-04 09:55:57 -0500

    Added v_prev

--- adex_dynamics.py
+++ adex_dynamics.py
@@ -1,7 +1,8 @@
 from imports import *
+from gpu_utils import *
 from parameters import par
 
-def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_prev, syn_u_prev, h, h_prev, con_dict, eff_var, var_dict):
+def calculate_dynamics(prev_eps, st, input_data, spikes, psuedo_der, syn_x, syn_u, con_dict, eff_var, var_dict, t):
 	""" Calculate the dynamics of the model
 		prev_eps = the epsilons of the previous time step
 		st       = dict holding all elements of model state
@@ -27,11 +28,13 @@ def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_pr
 
 	# Expand shapes to match [batch x presynaptic x postsynaptic]
 	# in preparation for outer products and broadcasting
-	z            = z[:,cp.newaxis,:]
-	z_prev       = z_prev[:,:,cp.newaxis]
-	x_prev       = x_prev[:,:,cp.newaxis]
-	z_prev_prev  = z_prev_prev[:,:,cp.newaxis]
-	h_prev       = h_prev if par['full_derivative'] else 0.
+	z            = spikes[t, :, cp.newaxis, :]
+	z_prev       = spikes[t-par['latency'], ...,cp.newaxis]
+	z_prev_prev  = spikes[t-2*par['latency'], ..., cp.newaxis]
+	x	         = input_data[t, ...,cp.newaxis]
+	x_prev       = input_data[t-par['latency'], ...,cp.newaxis]
+	h            = psuedo_der[t, :, cp.newaxis, :]
+	h_prev       = psuedo_der[t-par['latency'], ..., cp.newaxis] if par['full_derivative'] else 0.
 
 	# Cache common or unwieldy terms for readability and efficiency
 	dt_over_C      = c['dt']/c['C']
@@ -41,6 +44,11 @@ def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_pr
 	one_minus_beta = 1 - c['beta']
 
 	d_eff_weights_raw_weights = ((var_dict['W_rnn'] >= 0) * con_dict['EI_vector'][:,cp.newaxis])[cp.newaxis,...]
+	d_eff_weights_raw_weights = con_dict['EI_vector'][cp.newaxis,:,cp.newaxis]
+	#import matplotlib.pyplot as plt
+	#plt.imshow(to_cpu((var_dict['W_rnn'] >= 0) *con_dict['EI_vector'][:,cp.newaxis]), aspect='auto')
+	#plt.colorbar()
+	#plt.show()
 
 	one_minus_z           = 1. - z
 	one_minus_z_dt_over_C = one_minus_z * dt_over_C
@@ -50,6 +58,7 @@ def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_pr
 	exp_v_minus_one = cp.exp((v-c['V_T'])/c['D'])-1
 	#exp_v_minus_one = cp.minimum(1., exp_v_minus_one)
 	exp_v_minus_one = cp.clip(exp_v_minus_one, -1., 1.)
+	#exp_v_minus_one = cp.ones_like(exp_v_minus_one)
 	eps_dyn_v = exp_v_minus_one
 
 	adex_voltage_dvdv = one_minus_z*(1 + dt_g_over_C*eps_dyn_v)
@@ -57,8 +66,8 @@ def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_pr
 
 	# Set up new epsilon recording
 	eps = {}
-	eps['inp'] = {}
-	eps['rec'] = {}
+	eps['inp'] = {'prev_v': prev_eps['inp']['prev_v']}
+	eps['rec'] = {'prev_v': prev_eps['rec']['prev_v']}
 
 	### Update input epsilons
 	eps['inp']['v'] = \
@@ -76,13 +85,14 @@ def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_pr
 
 	### Update recurrent epsilons
 	eps['rec']['v'] = \
-		  prev_eps['rec']['v']  * adex_voltage_dvdv \
-		- prev_eps['rec']['w']  * one_minus_z_dt_over_C \
+		  prev_eps['rec']['v'] * adex_voltage_dvdv \
+		- prev_eps['rec']['w'] * one_minus_z_dt_over_C \
 		+ prev_eps['rec']['ir'] * one_minus_z_dt_mu_over_C
 
+
 	eps['rec']['w'] = \
-		  prev_eps['rec']['v']  * (dt_a_over_tau + c['b']*h) \
-		+ prev_eps['rec']['w']  * (1 - dt_over_tau)
+		  prev_eps['rec']['v'] * (dt_a_over_tau + c['b']*h) \
+		+ prev_eps['rec']['w'] * (1 - dt_over_tau)
 
 	eps['rec']['ir'] = \
 		  prev_eps['rec']['ir'] * c['beta'] \
@@ -98,12 +108,26 @@ def calculate_dynamics(prev_eps, st, x, x_prev, z, z_prev, z_prev_prev, syn_x_pr
 		  prev_eps['rec']['su'] * (1 - con_dict['alpha_stf'] - con_dict['U']*z_prev)
 
 	### Second-order terms
-	if False:
-		# Needs v_prev to work!
-		eps['rec']['v'] += \
-			  prev_eps['rec']['v_prev'] * h_prev \
-			* ((-1 + one_minus_beta * eff_var['W_rnn'][cp.newaxis,:,:]) * syn_x * syn_u - con_dict['U'] * (1 - syn_u))
+	"""
+	# dI/dz * dZ/dV
+	term_I = one_minus_beta * h_prev * syn_x * syn_u * eff_var['W_rnn'][cp.newaxis,:,:]
+	# dSx/dz * dZ/dV
+	term_Sx = -syn_x * syn_u * h_prev
+	# dSu/dz * dZ/dV
+	term_Su = con_dict['U'] * (1 - syn_u) * h_prev
 
+	eps['rec']['v'] += cp.einsum('bij,bjk->bik', prev_eps['rec']['prev_v'][0], term_I + term_Sx + term_Su)
+	eps['inp']['v'] += cp.einsum('bij,bjk->bik', prev_eps['inp']['prev_v'][0], term_I + term_Sx + term_Su)
+	"""
+
+	#eps['inp']['v'] += \
+	#	prev_eps['inp']['prev_v'][0] * (term_I + term_Sx + term_Su)
+
+	"""
+	eps['rec']['v'] += \
+		prev_eps['rec']['prev_v'][0] * h_prev \
+		* ((-1 + one_minus_beta * eff_var['W_rnn'][cp.newaxis,:,:]) * syn_x * syn_u - con_dict['U'] * (1 - syn_u))
+	"""
 
 	### Second-order corrections to recurrent epsilons
 	"""
